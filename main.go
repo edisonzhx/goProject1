60053e54e7d2a17a7244f0cb1e1365e67966ad71
edz <736641365@qq.com>
2022-05-04 20:58:53 +0800

    master commit

--- main.go
+++ main.go
@@ -6,5 +6,6 @@ func main() {
 	fmt.Println("good morning golangv1")
 	fmt.Println("good morning golangv2")
 	fmt.Println("good morning golangv3")
-	fmt.Println("bug fix")
+	fmt.Println("master 我也修改了第八行 ")
+	fmt.Println("master 我也修改了第九行 ")
 }
