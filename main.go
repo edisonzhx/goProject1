1bd39e5434d0c7554e51e8f377545f7e075f501d
edz <736641365@qq.com>
2022-05-04 20:58:08 +0800

    bug fix commit3

--- main.go
+++ main.go
@@ -6,5 +6,6 @@ func main() {
 	fmt.Println("good morning golangv1")
 	fmt.Println("good morning golangv2")
 	fmt.Println("good morning golangv3")
-	fmt.Println("bug fix")
+	fmt.Println("bug fix 我又修改了")
+	fmt.Println("bug fix 我又修改了第九行")
 }
